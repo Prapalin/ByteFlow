b61586f46cd491ea99fa30cd6bfb5d842a8146d1
Prapalin <mishraprapalin@gmail.com>
2026-07-16 12:18:14 +0530

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
+export default function Navbar(){
+    const [mobileIsOpen, setMobileIsOpen] = useState(false);
+    return <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-900/20 backdrop-blur-sm">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
+                <div className="flex items-center space-x-1 group cursor-pointer">
+                    <div>
+                        <img src="./logo.png" alt="logo" className="w-6 h-6 sm:w-8 sm:h-8" />
+                    </div>
+                    <span className="text-lg sm:text-xl md:text-2xl font-medium">
+                        <span className="text-blue-400">Nova</span>
+                        <span className="text-white">UI</span>
+                    </span>
+                </div>
+                {/* Nav links */}
+                <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
+                    <a href="#features" className="text-gray-300 hover:text-white text-sm lg:text-base">Features</a>
+                    <a href="#pricing" className="text-gray-300 hover:text-white text-sm lg:text-base">Pricing</a>
+                    <a href="#testimonials" className="text-gray-300 hover:text-white text-sm lg:text-base">Testimonials</a>
+                </div>
+
+                <button className="md:hidden p-2 text-gray-300 hover:text-white"
+                    onClick={()=> setMobileIsOpen((prev) => !prev)}>
+                    {mobileIsOpen ? (<X className="w-5 h-5 sm:w-6 sm:h-6 hover:cursor-pointer"/>) : (
+                    <Menu className="w-5 h-5 sm:w-6 sm:h-6 hover:cursor-pointer"/>
+                    )}
+                </button>
+            </div>
+        </div>
+
+        {mobileIsOpen && (<div className="md:hidden bg-slate-900/95 backdrop-blur-lg border-t border-slate-800 animate-in slide-in-from-top animation-duration">
+            <div className="px-4 py-4 sm:py-6 space-y-3 sm:space-y-4">
+                <a
+                onClick={()=> setMobileIsOpen(false)}
+                href="#features" className="block text-gray-300 hover:text-white text-sm lg:text-base">Features</a>
+                <a 
+                onClick={()=> setMobileIsOpen(false)}
+                href="#pricing" className="block text-gray-300 hover:text-white text-sm lg:text-base">Pricing</a>
+                <a 
+                onClick={()=> setMobileIsOpen(false)}
+                href="#testimonials" className="block text-gray-300 hover:text-white text-sm lg:text-base">Testimonials</a>
+            </div>
+        </div>)}
+    </nav>
+}
