a18858c1cedc24a981af9ce75afb4463b09dc559
Prapalin <mishraprapalin@gmail.com>
2026-07-20 00:09:09 +0530

    Added Pricing, Testimonials and Footer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -114,7 +114,17 @@ export default function Hero(){
 
                                 {/* Code Content */}
                                 <div className="relative overflow-hidden flex-grow">
-                                    <SyntaxHighlighter language="javascript" style={nightOwl} customStyle={{margin : 0, borderRadius : "8px", fontSize : "11px", lineHeight: "1.4", height: "100%", border : "1px solid #3c3c3c",}}>
+                                    <SyntaxHighlighter language="javascript" style={nightOwl} customStyle={
+                                        {margin : 0,
+                                         borderRadius : "8px",
+                                         fontSize : "11px",
+                                         lineHeight: "1.4",
+                                         height: "100%",
+                                         border : "1px solid #3c3c3c",
+                                         wordWrap : "break-word",
+                                         whiteSpace : "pre-wrap",
+                                         textAlign : "left",
+                                         }}>
                                         {codeExamples[activeTab]}
                                     </SyntaxHighlighter>
                                 </div>
